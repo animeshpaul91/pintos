9fbeaa674eb6b6e68bec20784653dfe8b6b398e5
Animesh <ani.nitmz@gmail.com>
2019-11-20 11:40:19 -0500

    UnCommented Parent-parent-sema

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -576,7 +576,7 @@ init_thread (struct thread *t, const char *name, int priority)
   #ifdef USERPROG
   t->parent = running_thread();
   sema_init(&t->parent_sema, 0);
-  //sema_init(&t->parent->parent_sema, 0);
+  sema_init(&t->parent->parent_sema, 0);
   #endif
   //Added Code Ends
 
