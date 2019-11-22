cafb91120ae7c7a28174cac1964174767718e3b2
Animesh <ani.nitmz@gmail.com>
2019-11-22 18:30:26 -0500

    Debugging process_wait()

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -101,22 +101,18 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  /* printf("\nEntered Process Wait\n");
   struct thread *parent = thread_current(), *child = get_thread_with_tid(child_tid);
-  printf("\n%s has to wait for tid = %d\n",parent->name ,child_tid);
   struct child_exit_status *exiting_child = NULL;
   struct list_elem *l;
-  printf("\nTesting 1\n");
-  struct list my_child_list = parent->child_list;
-  printf("\nTesting 2\n");
+  //struct list my_child_list = parent->child_list;
   int status = -1;
 
   if (child != NULL && child->parent == parent) //If child is found and parent is the calling thread 
     sema_down(&parent->parent_sema);
   
-  if (!list_empty(&my_child_list))  //Iterate through Parent's dead children 
+  if (!list_empty(&parent->child_list))  //Iterate through Parent's dead children 
   {
-    for (l = list_begin(&my_child_list); l != list_end(&my_child_list); l = list_next(l))
+    for (l = list_begin(&parent->child_list); l != list_end(&parent->child_list); l = list_next(l))
     {
       exiting_child = list_entry(l, struct child_exit_status, elem);
       if (child_tid == exiting_child->tid)
@@ -130,21 +126,17 @@ process_wait (tid_t child_tid UNUSED)
       free(exiting_child);
     }
   }
-  return status; */
+  return status;
 
-  //Added
-  struct thread *c_thread = get_thread_with_tid(child_tid), *p_thread = thread_current();
+  /* struct thread *c_thread = get_thread_with_tid(child_tid), *p_thread = thread_current();
   struct child_exit_status *ces = NULL;
   int status = -1;
 
-  // printf("%s has to wait for tid=%d\n",p_thread->name ,child_tid);
-  //If child found and parent is caller, wait
   if (c_thread != NULL && c_thread->parent == p_thread)
   {
-    // printf("Waiting for child_tid %d \n", child_tid);
     sema_down(&p_thread->parent_sema);
   }
-  //If the thread has already terminated and is a child
+  
   if (!list_empty(&p_thread->child_list))
   {
     for (struct list_elem *l = list_begin(&p_thread->child_list); l != list_end(&p_thread->child_list); l = list_next(l))
@@ -153,7 +145,7 @@ process_wait (tid_t child_tid UNUSED)
       if (child_tid == ces->tid)
         break;
     }
-    //If child was found, read it and remove from the list
+    
     if (ces->tid == child_tid)
     {
       status = ces->exit_status;
@@ -161,9 +153,7 @@ process_wait (tid_t child_tid UNUSED)
       free(ces);
     }
   }
-  // printf("Status is %d\n",status);
-  //TODO free CES
-  return status;
+  return status; */
 }
 
 /* Free the current process's resources. */
