45cc03c1f8543e969db23f0055a3938d545b74fe
Animesh <ani.nitmz@gmail.com>
2019-11-29 20:19:55 -0500

    Added exit(-1) in exception.c

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,9 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+//Added Code begins
+#include "userprog/syscall.h"
+//Added code Ends
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -151,6 +154,11 @@ page_fault (struct intr_frame *f)
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
+
+  /* Added Code begins */ 
+  exit(-1);
+  /* Added Code ends */ 
+  
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
